552b9abdba478f827544a25416a8310e321aace6
Niladri-Gudu <niladrigudu@gmail.com>
2026-05-03 17:37:44 +0530

    prisma setup done

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,16 +1,56 @@
 import Fastify from "fastify";
+import "dotenv/config";
+import { prisma } from "./lib/prisma.ts";
 
-const fastify = Fastify({
+const app = Fastify({
   logger: true,
 });
 
-fastify.get("/", function (request, reply) {
+const PORT = parseInt(process.env.PORT || "3000", 10);
+
+app.get("/", function (request, reply) {
   reply.send("hello world");
 });
 
-fastify.listen({ port: 8000 }, function (err, address) {
-  if (err) {
-    fastify.log.error(err);
+app.post(
+  "/users",
+  {
+    schema: {
+      body: {
+        type: "object",
+        required: ["email", "password"],
+        properties: {
+          email: { type: "string", format: "email" },
+          password: { type: "string", minLength: 8 },
+        },
+      },
+    },
+  },
+  async (request, reply) => {
+    const { email, password } = request.body as {
+      email: string;
+      password: string;
+    };
+
+    const user = await prisma.user.create({
+      data: {
+        email,
+        passwordHash: password,
+      },
+    });
+
+    return user;
+  },
+);
+
+const start = async () => {
+  try {
+    await app.listen({ port: PORT });
+  } catch (error) {
+    app.log.error(error);
     process.exit(1);
   }
-});
+  console.log(`Server is running on port ${PORT}`);
+};
+
+start();
